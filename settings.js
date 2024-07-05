2ebf761e44d5e6629cd27cd95f825adf61734a81
yanzzhostzzz <ryanzzdd3@gmail.com>
2024-07-05 19:46:44 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
+global.creator = 'YANZZ BOT'// yourname
 global.MONGO_DB_URI = "" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "ridzzyanzz@gmail.com" //email
+global.email_password = "yanzzstore" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
